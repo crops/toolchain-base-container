ca1566d84b292a0c028973204581e65f5a6b5c49
brian avery <brian.avery@intel.com>
2016-05-27 16:07:28 -0700

    what if I try a PR with naughty changes

--- scripts/pushToDockerhub.sh
+++ scripts/pushToDockerhub.sh
@@ -28,13 +28,12 @@ fi
 docker login -e $DOCKER_EMAIL -u $DOCKERHUB_USER -p $DOCKER_PASS
 
 docker images | grep ${DOCKERHUB_REPO}\/ | grep -v deps | grep -v \<none\> | awk '{print $1 ":" $2}'
-if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-    # l8r also check $TRAVIS_REPO_SLUG so we only push to dockerhub on master
-    for i in `docker images | grep ${DOCKERHUB_REPO}\/ | grep -v deps | grep -v \<none\> | awk '{print $1 ":" $2}'`; do
-	echo Pushing $i
-	docker push $i
-    done
-else
-    print "No Pushy, TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST"
-
-fi
+#if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+# l8r also check $TRAVIS_REPO_SLUG so we only push to dockerhub on master
+for i in `docker images | grep ${DOCKERHUB_REPO}\/ | grep -v deps | grep -v \<none\> | awk '{print $1 ":" $2}'`; do
+    echo Pushing $i
+    docker push $i
+done
+#else
+#    print "No Pushy, TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST"
+#fi

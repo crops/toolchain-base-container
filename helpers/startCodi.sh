5c6ba268a7c8fe1badaa72e03c1dac0783a6ed7b
bavery <brian.avery@intel.com>
2016-06-20 14:32:06 -0700

    helpers: tail the rethinkdb log file for docker log
    Signed-off-by: bavery <brian.avery@intel.com>

--- helpers/startCodi.sh
+++ helpers/startCodi.sh
@@ -3,3 +3,7 @@
 /etc/init.d/rethinkdb start
 sleep 5
 python3 -m launchers.codi-launcher
+# in case things went poorly, this tail
+# will give us info rather than having a stopped
+# container
+tail -f /var/lib/rethinkdb/default/data/log_file
